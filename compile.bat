eeca19a58feecd0814fa4dcc5bb8b0f7334b26e9
not-nef <tom.hessi@web.de>
2022-07-18 17:18:17 +0200

    fix build script

--- compile.bat
+++ compile.bat
@@ -2,4 +2,4 @@ rmdir /Q /S dist
 pyinstaller ./src/main.py --onefile --windowed --collect-data sv_ttk --icon "./assets/logo.ico"
 rmdir /Q /S build
 rmdir /Q /S __pycache__
-del /Q /S txt2.spec
+del /Q /S main.spec
