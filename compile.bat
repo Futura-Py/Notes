2edba234b263810377bb7e7a8bdc2ea733a0cbfc
not-nef <tom.hessi@web.de>
2022-09-22 18:18:59 +0200

    hopefully fix build

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
 rmdir /Q /S dist
-pyinstaller ./src/main.py --onefile --windowed --collect-data sv_ttk --icon "./assets/logo.ico"
+pyinstaller ./src/main.py --onefile --windowed --collect-data sv_ttk --collect-all tkinterweb --icon "./assets/logo.ico"
 rmdir /Q /S build
 rmdir /Q /S __pycache__
 del /Q /S main.spec
